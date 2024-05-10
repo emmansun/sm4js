f433016302fcdbda31e4208d1aede5c9c78a5be7
Sun Yimin <emmansun@users.noreply.github.com>
2024-05-10 16:38:52 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   ...require('./src/asn1'),
   ...require('./src/sm3'),
-  ...require('./src/bytescodeHex'),
+  ...require('./src/bytescodecHex'),
   ...require('./src/kdf'),
   ...require('./src/sm4'),
   ...require('./src/sm2')
